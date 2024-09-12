bf7f4e57a9ccd405fb17d142677b7600b6e72b8b
spaceotti <spaceotti1981@gmail.com>
2024-09-12 10:13:21 +0200

    Bugs fixed, finished the todo app

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,40 @@ import ListItem from "./model/ListItem";
 import FullList from "./model/FullList";
 import ListTemplate from "./templates/ListTemplate";
 
-const initApp = (): void {
-    const fullList = FullList.instance;
-    const template = ListTemplate.instance;
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
 
-    const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent): void => {
+    e.preventDefault();
 
-    const clearItems = document.getElementById('')
-}
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+    if (!newEntryText.length) return;
 
-document.addEventListener('DOMContentLoaded', initApp)
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1;
+
+    const newItem: ListItem = new ListItem(itemId.toString(), newEntryText);
+    fullList.addItem(newItem);
+
+    template.render(fullList);
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
