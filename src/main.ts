2b47e6b6e29acbeb7640a97978a572ed56f7228f
spaceotti <spaceotti1981@gmail.com>
2024-09-12 09:45:26 +0200

    Implenenting initApp function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./css/style.css";
+import ListItem from "./model/ListItem";
+import FullList from "./model/FullList";
+import ListTemplate from "./templates/ListTemplate";
+
+const initApp = (): void {
+    const fullList = FullList.instance;
+    const template = ListTemplate.instance;
+
+    const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement
+
+    const clearItems = document.getElementById('')
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
